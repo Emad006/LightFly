7eb7c261f121025ad222b69db71657cce40ca089
XyroPhyte <emadharun.eh@gmail.com>
2021-07-01 17:08:11 +0600

    Registered onJoin Listener

--- src/main/java/me/xyrophyte/lightfly/LightFly.java
+++ src/main/java/me/xyrophyte/lightfly/LightFly.java
@@ -2,6 +2,7 @@ package me.xyrophyte.lightfly;
 
 import me.xyrophyte.lightfly.commands.Fly;
 import me.xyrophyte.lightfly.events.OnDamage;
+import me.xyrophyte.lightfly.events.OnJoin;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -17,6 +18,7 @@ public final class LightFly extends JavaPlugin {
         getLogger().info(ChatColor.LIGHT_PURPLE + "https://discord.gg/MmqXcebnxC");
         Objects.requireNonNull(getCommand("fly")).setExecutor(new Fly(this));
         getServer().getPluginManager().registerEvents(new OnDamage(), this);
+        getServer().getPluginManager().registerEvents(new OnJoin(), this);
         getLogger().info(ChatColor.AQUA + "Plugin started successfully!");
     }
 
